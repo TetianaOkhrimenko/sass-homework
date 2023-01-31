f81d6b2372a83c13c1a0aa1f17bca0eec40d5e9b
TetianaOkhrimenko <TANOH@ukr.net>
2023-01-31 08:03:23 +0000

    Changed gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,62 @@
-var gulp = require("gulp");
-var postcss = require("gulp-postcss");
-var sass = require("gulp-sass")(require("sass"));
+import gulp from "gulp";
+import sass from "gulp-sass";
+import sourcemaps from "gulp-sourcemaps";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+// import mqpacker from 'css-mqpacker';
+import config from "../config";
+import csso from "postcss-csso";
 
-var autoprefixer = require("autoprefixer");
-var cssnano = require("cssnano");
+const isMax = (mq) => /max-width/.test(mq);
+const isMin = (mq) => /min-width/.test(mq);
 
-var sassPath = "./sass/**/*.{sass, scss}";
+const sortMediaQueries = (a, b) => {
+  A = a.replace(/\D/g, "");
+  B = b.replace(/\D/g, "");
 
-gulp.task("css", function () {
-  var processors = [autoprefixer(), cssnano];
-  return gulp
-    .src(sassPath)
-    .pipe(sass().on("error", sass.logError))
+  if (isMax(a) && isMax(b)) {
+    return B - A;
+  } else if (isMin(a) && isMin(b)) {
+    return A - B;
+  } else if (isMax(a) && isMin(b)) {
+    return 1;
+  } else if (isMin(a) && isMax(b)) {
+    return -1;
+  }
+  return 1;
+};
+
+const processors = [
+  autoprefixer({
+    overrideBrowserslist: ["last 4 versions"],
+    cascade: false,
+  }),
+  // require('lost'),
+  // mqpacker({
+  //   sort: sortMediaQueries
+  // }),
+  csso,
+];
+
+gulp.task("sass", () =>
+  gulp
+    .src(config.src.sass + "/*.{sass,scss}")
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        outputStyle: config.production ? "compact" : "expanded", // nested, expanded, compact, compressed
+        precision: 5,
+      })
+    )
+    .on("error", config.errorHandler)
     .pipe(postcss(processors))
-    .pipe(gulp.dest("./public/css"));
-});
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(config.dest.css))
+);
 
-gulp.task("watch:sass", function () {
-  return gulp.watch(sassPath, gulp.series("css"));
-});
+const build = (gulp) => gulp.parallel("sass");
+const watch = (gulp) => () =>
+  gulp.watch(config.src.sass + "/**/*.{sass,scss}", gulp.parallel("sass"));
+
+module.exports.build = build;
+module.exports.watch = watch;
